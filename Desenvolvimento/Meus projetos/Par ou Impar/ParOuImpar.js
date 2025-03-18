c43249768804f4cfa25f64df85f6781844b5d505
JoãoFL <joaofilip444@gmail.com>
2025-03-17 22:33:37 -0300

    Add files via upload

---
+++ Desenvolvimento/Meus projetos/Par ou Impar/ParOuImpar.js
@@ -0,0 +1,27 @@
+const numeroJS = document.getElementById("numero")
+const mensagemJS = document.getElementById("mensagem")
+
+
+// Declarar i incializar a variavel numero
+
+var numero = 0;
+
+// Declarar i incializar a variavel mensagem
+var mensagem = "";
+
+// Declarar i incializar a variavel resto
+var resto = 0;
+
+// Coletar o valor digitado no numero
+numero = numeroJS.
+
+//  Dividir o numero por 2 e obter o resto
+resto =numero%2;
+
+// Gerar a mensagem com base no conteldo do resto
+if (resto = 0) {
+    numero = "O numero informado é PAR"
+}
+else {
+    numero = "O numero informado é IMPAR"
+}
